83c7447f713d423a757b6eb0565ddd8723da02a1
phongsakorn <phongsakorn801@gmail.com>
2023-07-19 22:28:00 +0700

    ทำให้ สามารถเข้าถึงเฉพาะหน้าของ position นั้นๆ

--- angularForm/src/app/services/users/auth-guard.service.ts
+++ angularForm/src/app/services/users/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -10,14 +10,31 @@ export class AuthGuardService implements CanActivate {
 
   constructor(
     private authService: AuthService,
-    private router: Router  
+    private router: Router,
   ) { }
 
-  canActivate(): boolean {
-    if (this.authService.getLogin()) {
-      return true;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+
+    const currentUserPosition = this.authService.getPos();
+
+    // ตรวจสอบว่าเมื่อเข้าถึงหน้า dashboard ผู้ใช้งานต้องเป็น admin เท่านั้น
+    if (route.routeConfig && route.routeConfig.path) {
+      if (route.routeConfig.path === "dashboard" && currentUserPosition !== "2") {
+        this.router.navigate(['/login']);
+        return false;
+      }
+
+      if (route.routeConfig.path === "company" && currentUserPosition !== "1") {
+        this.router.navigate(['/login']);
+        return false;
+      }
     }
-    this.router.navigate(['/login']);
-    return false;
+
+    return true;
+    // if (this.authService.getLogin()) {
+    //   return true;
+    // }
+    // this.router.navigate(['/login']);
+    // return false;
   }
 }
